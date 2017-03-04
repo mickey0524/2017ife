94103e1fda72ff4737ae476fb53561e9cadceb35
baihao <buptbh@163.com>
2017-03-04 15:27:36 +0800

    完成对象深度的getter和setter设定以及创建自定义对象对需要的属性进行监听变化

---
+++ vue-learning-2/task.js
@@ -0,0 +1,101 @@
+
+/**
+ * @params {obj} data 传入的对象
+ */
+function Observer (data) {
+    this.data = data;
+    this.makeObserver(data);
+    this.event = new Event();
+}
+
+/**
+ * @params {String} key 对象的key
+ * @params {string/number/obj} value 对象对应key的值
+ */
+Observer.prototype.setterAndGetter = function(key, value) {
+	var _this = this;
+    Object.defineProperty(this.data, key, {
+        get: function(){
+            console.log('你访问了' + key);
+            return value;
+    	},
+    	set: function(newVal){
+      		console.log('你设置了' + key);
+      		console.log('新的' + key + '=' + newVal);
+      		_this.event.emit(key, value, newVal);
+      		value = newVal;
+      		if(typeof newVal == 'object') {
+      			new Observer(value);
+      		}
+    	}
+  	});
+}
+
+/**
+ * @params {object} obj 对象
+ */
+Observer.prototype.makeObserver = function(obj) {
+  for(var key in obj) {
+      if(typeof obj[key] === 'object'){
+        new Observer(obj[key]);
+      }
+    this.setterAndGetter(key, obj[key]);
+  }
+}
+
+/**
+ * @params {property} attr 需要监听改变的数据
+ * @params {function} callback 处理数据改变的回调函数
+ */
+Observer.prototype.$watch = function(attr, callback) {
+	this.event.on(attr, callback);
+}
+
+/**
+ * 自定义事件处理程序
+ */
+function Event() {
+	this.event = {};
+}
+
+/**
+ * @params {property} attr 需要监听改变的数据
+ * @params {function} callback 处理数据改变的回调函数 
+ */
+Event.prototype.on = function(attr, callback) {
+	if(this.event[attr]) {
+		this.event[attr].push(callback);
+	}
+	else {
+		this.event[attr] = [callback];
+	}
+}
+
+/**
+ * @params {property} attr 需要监听改变的数据
+ * @params {string/int/object} 对象属性原先的值
+ * @params {string/int/object} 对象属性改变后的值
+ */
+Event.prototype.emit = function(attr, oldVal, newVal) {
+	this.event[attr] && this.event[attr].forEach(function(item) {
+		item(oldVal, newVal);
+	});
+}
+
+var app = new Observer({
+	a : 3,
+	b : 4,
+	c : {
+		d : 5,
+		e : 6
+	}
+});
+
+app.$watch('b', function(oldVal, newVal) {
+	console.log('b原来的值是' + oldVal + '，现在的值是' + newVal);
+})
+
+app.data.a;
+app.data.c.d;
+app.data.b = 19;
+app.data.c.e = 11;
