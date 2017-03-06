03b8e273f6a8fcbdb66db9527007033400e46640
baihao <buptbh@163.com>
2017-03-06 20:43:30 +0800

    得到新的思路

--- 正则表达式入门/task.js
+++ 正则表达式入门/task.js
@@ -9,3 +9,9 @@ var repeatStr = /([a-zA-Z]+\s)\1/g;
 console.log(repeatStr.exec('foo foo bar '));
 console.log(repeatStr.test('foo bar foo '));
 console.log(repeatStr.exec('bar barbar '));
+
+var repeatStr_1 = /(^|\s)([a-zA-Z]+)\s+\2\b/g;
+
+console.log(repeatStr_1.exec('foo foo bar'));
+console.log(repeatStr_1.test('foo bar foo'));
+console.log(repeatStr_1.exec('bar barbar'));
